3e5729957f2cb1f3fc66da0abfa83913517256fe
LamiIIIa <laminoji@gmail.com>
2026-08-16 19:45:28 +0300

    some fix

---
+++ src/components/linksObgect.js
@@ -0,0 +1,19 @@
+export default lonkObj = {
+  linkForMobHead: {
+    maketLD: {
+        link: "https://akatsukigood.forum.cool/viewtopic.php?id=68#p245",
+        title: 'Шаблон личного дела',
+    },
+    storyline: {
+        link: "https://akatsukigood.forum.cool/viewtopic.php?id=99",
+        title: 'Сюжет',
+    },
+    looking: {
+        link: "https://akatsukigood.forum.cool/viewtopic.php?id=99",
+        title: 'Заявки на роли',
+    },
+    calc: {
+        link: "https://akatsukigood.forum.cool/pages/kalkulyator"
+        title: 'Калькулятор'},
+  },
+};
