19ce2889e667072df3752daa177f9b1244b47cd3
LamiIIIa <laminoji@gmail.com>
2026-08-16 20:24:11 +0300

    some fix

--- src/components/header/mobile-important-links.js
+++ src/components/header/mobile-important-links.js
@@ -5,5 +5,38 @@ export function initMobileImportantLink(header) {
 
   const importantLinks = Object.values(linkObj.linkForMobHead);
 
-  const
+  const container = document.createElement("div");
+  container.className = "mobile-important-links";
+
+  const button = document.createElement(button);
+  button.classname = "mobile-important-links-buttun";
+  button.type = "button";
+
+  button.setAttribute("aria-label", "Открыть важные ссылки");
+  button.setAttribute("aria-controls", "mobile-important-links-menu");
+  button.setAttribute("aria-expanded", "false");
+
+  const menu = document.querySelector("div");
+  menu.id = "mobile-important-links-menu";
+  menu.className = "mobile-important-links__menu";
+  menu.setAttribute("aria-label", "Важные ссылки");
+  menu.hidden = true;
+
+  const list = document.createElement("ul");
+  list.className = "mobile-important-links__list";
+
+  importantLinks.forEach(({ title, link }) => {
+    const item = document.createElement("li");
+    item.className = "mobile-important-links__item";
+
+    const anchor = document.createElement("a");
+    anchor.className = "mobile-important-links__link";
+    anchor.href = link;
+    anchor.textContent = title;
+
+    item.append(anchor);
+    list.append(item);
+  });
+
+  menu.append(list);
 }
