027ba2402dad3d6552545dc74de8b1e658fb7179
LamiIIIa <laminoji@gmail.com>
2026-08-19 18:21:54 +0300

    tables in posts fix

--- src/pages/topic-view.js
+++ src/pages/topic-view.js
@@ -1,12 +1,13 @@
-import { initMutualPromotion } from '../components/mutual-promotion.js'
+import { initMutualPromotion } from "../components/mutual-promotion.js";
 
 export function initTopicView({ main, topicId }) {
-  main?.classList.add('forum-design-topic')
-  if (topicId !== null) main?.setAttribute('data-topic-id', String(topicId))
+  main?.classList.add("forum-design-topic");
+  if (topicId !== null) main?.setAttribute("data-topic-id", String(topicId));
 
   main?.querySelectorAll('.post[id^="p"]').forEach((post, index) => {
-    post.dataset.postIndex = String(index)
-  })
+    post.dataset.postIndex = String(index);
+  });
 
-  initMutualPromotion(main)
+  initMutualPromotion(main);
+  initScrollablePostTables();
 }
