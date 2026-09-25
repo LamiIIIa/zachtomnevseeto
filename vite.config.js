5981c8468b07a919bb67333ee1d531d49293a46d
LamiIIIa <laminoji@gmail.com>
2026-09-25 22:00:23 +0300

    some fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  // Файлы HTML-постов не входят в общий бандл, поэтому публикуем их отдельно.
+  publicDir: 'prototype',
   build: {
     lib: {
       entry: 'src/main.js',
